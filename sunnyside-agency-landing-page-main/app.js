dd11fce6efb2da02c4d54c333759ab6597599142
ScottDeev <99127718+ScottDeev@users.noreply.github.com>
2022-03-19 15:11:34 +0100

    new

--- sunnyside-agency-landing-page-main/app.js
+++ sunnyside-agency-landing-page-main/app.js
@@ -1,13 +1,15 @@
 const navbarToggle = document.querySelector(".icon");
-const nav = document.querySelector("#navbar");
-const prev = window.pageYOffset;
+const nav = document.querySelector(".nav_details");
+const navbar = document.querySelector('.hide_menu');
 
 navbarToggle.addEventListener("click", function(){
     nav.classList.toggle("hide_menu");
-})
+    nav.style.transition = "all .8s"
 
-window.addEventListener("scroll", function(){
-    if(prev < 50){
-        nav.classList.add("hide_menu");
+})
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > screen.height) {
+      navbar.classList.add("hide_menu");
     }
 })
